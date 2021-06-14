d95a7c6d76b4a2b1810ebacd9b9666be5d5c445a
psb629 <psb629@gmail.com>
2021-06-14 17:47:58 +0900

    prepare to make conda env

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/zsh
 
 git_dir=~/Github
-branch=main
-
 work_dir=$git_dir/machine_learning
 cd $work_dir
+
+#### Github ####
+branch=main
+
 git init
 git config --global user.name "psb629"
 git config --global user.email "psb629@gmail.com"
@@ -22,3 +24,16 @@ echo "> * `users`(`ipconfig getifaddr en0`): `date`" >>$work_dir/README.md
 git add -A
 git commit -m "ran setup.sh"
 git push -u origin $branch
+
+#### anaconda ####
+env_name=sam_ML
+conda create -n $env_name python=3.6 ipykernel
+source activate $env_name
+is_env_activated=`conda info | grep $env_name | wc -l`
+if [ $is_env_activated -gt 0 ]; then
+	python -m ipykernel install --user --name $env_name --display-name $env_name
+	echo "ready to pip install modules at env '${env_name}'"
+	pip install -r $work_dir/requirements.txt
+else
+	echo "pip install modules at env '${env_name}' is failed"
+fi
